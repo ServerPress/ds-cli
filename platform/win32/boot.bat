01ea07e694587d94b17246e3749968e2af807cdd
Stephen J. Carnam <steve@steveorevo.com>
2015-10-07 00:24:31 -0700

    Restore system attrib on composer symbolic link

--- platform/win32/boot.bat
+++ platform/win32/boot.bat
@@ -2,5 +2,6 @@
 REM
 REM Boot native CLI
 REM
+attrib +s c:\xampplite\ds-plugins\ds-cli\platform\win32\cygwin\bin\composer
 SET PATH=c:\xampplite\php;c:\xampplite\mysql\bin;c:\xampplite\perl\bin;c:\xampplite\apache\bin;c:\xampplite\other;c:\xampplite\ds-plugins\ds-cli\platform\win32\cygwin\bin\;c:\xampplite\ds-plugins\ds-cli\vendor\wp-cli\wp-cli\bin;c:\xampplite\ds-plugins\ds-cli\vendor\bin;C:\xampplite\ds-plugins\ds-cli\platform\win32\nodejs\;%PATH%
 %*
