18ad5a7e5250148be6bdc585b315f37b91e69877
Stephen J. Carnam <steve@steveorevo.com>
2015-10-06 17:35:00 -0700

    Windows nodejs runtime

--- platform/win32/boot.bat
+++ platform/win32/boot.bat
@@ -2,5 +2,5 @@
 REM
 REM Boot native CLI
 REM
-SET PATH=c:\xampplite\php;c:\xampplite\mysql\bin;c:\xampplite\perl\bin;c:\xampplite\apache\bin;c:\xampplite\other;c:\xampplite\ds-plugins\ds-cli\platform\win32\cygwin\bin\;c:\xampplite\ds-plugins\ds-cli\vendor\wp-cli\wp-cli\bin;c:\xampplite\ds-plugins\ds-cli\vendor\bin;%PATH%
+SET PATH=c:\xampplite\php;c:\xampplite\mysql\bin;c:\xampplite\perl\bin;c:\xampplite\apache\bin;c:\xampplite\other;c:\xampplite\ds-plugins\ds-cli\platform\win32\cygwin\bin\;c:\xampplite\ds-plugins\ds-cli\vendor\wp-cli\wp-cli\bin;c:\xampplite\ds-plugins\ds-cli\vendor\bin;C:\xampplite\ds-plugins\ds-cli\platform\win32\nodejs\;%PATH%
 %*
