3cbbed81702dd62be0f65610d2310515fdf50c95
Stephen J. Carnam <steve@steveorevo.com>
2015-12-28 17:40:45 -0800

    Resolve certificate errors when using cURL on windows.

--- platform/win32/boot.bat
+++ platform/win32/boot.bat
@@ -3,5 +3,5 @@ REM
 REM Boot native CLI
 REM
 SET DS_CLI=c:\xampplite\ds-plugins\ds-cli
-PATH=%DS_CLI%\platform\all\pre;%DS_CLI%\platform\win32\pre;c:\xampplite\php;c:\xampplite\mysql\bin;c:\xampplite\perl\bin;c:\xampplite\apache\bin;c:\xampplite\other;%DS_CLI%\platform\win32\cygwin\bin;%DS_CLI%\vendor\wp-cli\wp-cli\bin;%DS_CLI%\platform\win32\nodejs\;%PATH%
+PATH=%DS_CLI%\platform\all\pre;%DS_CLI%\platform\win32\pre;%DS_CLI%\platform\win32\cygwin\bin;c:\xampplite\php;c:\xampplite\mysql\bin;c:\xampplite\perl\bin;c:\xampplite\apache\bin;c:\xampplite\other;%DS_CLI%\vendor\wp-cli\wp-cli\bin;%DS_CLI%\platform\win32\nodejs\;%PATH%
 %*
