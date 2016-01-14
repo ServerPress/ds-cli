d2ff6a842a65f3e42ca60c4d02a27735039241e5
Stephen J. Carnam <steve@steveorevo.com>
2016-01-13 23:23:45 -0800

    Includes npm, nodejs, gulp, grunt global path definitions.

--- platform/mac/boot.sh
+++ platform/mac/boot.sh
@@ -5,7 +5,9 @@
 unset HISTFILE;
 export DS_CLI="/Applications/XAMPP/ds-plugins/ds-cli"
 export WGETRC="$DS_CLI/platform/mac/homebrew/etc/wgetrc"
+export npm_config_cache="$DS_CLI/platform/mac/npm-cache"
+export npm_config_prefix="$DS_CLI/platform/mac/npm"
 export DYLD_FALLBACK_LIBRARY_PATH="$DS_CLI/platform/mac/homebrew/lib:/Applications/XAMPP/xamppfiles/lib:/usr/lib"
-export PATH="$DS_CLI/platform/all/pre:$DS_CLI/platform/mac/pre:$DS_CLI/vendor/bin:$DS_CLI/platform/mac/homebrew/bin:/Applications/XAMPP/xamppfiles/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:$PATH"
+export PATH="$DS_CLI/platform/all/pre:$DS_CLI/platform/mac/pre:$DS_CLI/vendor/bin:$DS_CLI/platform/mac/homebrew/bin:$DS_CLI/platform/mac/npm/bin:/Applications/XAMPP/xamppfiles/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:$PATH"
 unset DYLD_LIBRARY_PATH
 "$@"
