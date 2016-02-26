2d76426af0ec42e2d85de116b0a148a6b77a5767
Stephen J. Carnam <steve@steveorevo.com>
2016-02-25 17:28:03 -0800

    Define COMPOSER_HOME for our unique instance.

--- platform/mac/boot.sh
+++ platform/mac/boot.sh
@@ -9,6 +9,7 @@ export GIT_EXEC_PATH="$DS_CLI/platform/mac/homebrew/Cellar/git/2.7.0/libexec/git
 export GIT_TEMPLATE_DIR="$DS_CLI/platform/mac/homebrew/Cellar/git/2.7.0/share/git-core/templates"
 export npm_config_cache="$DS_CLI/platform/mac/npm-cache"
 export npm_config_prefix="$DS_CLI/platform/mac/npm"
+export COMPOSER_HOME="$DS_CLI/platform/mac/composer"
 export DYLD_FALLBACK_LIBRARY_PATH="$DS_CLI/platform/mac/homebrew/lib:/Applications/XAMPP/xamppfiles/lib:/usr/lib"
 export PATH="$DS_CLI/platform/all/pre:$DS_CLI/platform/mac/pre:$DS_CLI/vendor/bin:$DS_CLI/platform/mac/homebrew/bin:$DS_CLI/platform/mac/npm/bin:/Applications/XAMPP/xamppfiles/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:$PATH"
 unset DYLD_LIBRARY_PATH
--- platform/win32/boot.bat
+++ platform/win32/boot.bat
@@ -5,6 +5,7 @@ REM
 SET DS_CLI=c:\xampplite\ds-plugins\ds-cli
 SET npm_config_cache=%DS_CLI%\platform\win32\npm-cache
 SET npm_config_prefix=%DS_CLI%\platform\win32\npm
+SET COMPOSER_HOME=%DS_CLI%\platform\win\composer
 SET CYGWIN_HOME=%DS_CLI%\platform\win32\cygwin
 PATH=%DS_CLI%\platform\all\pre;%DS_CLI%\platform\win32\pre;%DS_CLI%\platform\win32\cygwin\bin;%DS_CLI%\platform\win32\nodejs;%DS_CLI%\platform\win32\npm;c:\xampplite\php;c:\xampplite\mysql\bin;c:\xampplite\perl\bin;c:\xampplite\apache\bin;c:\xampplite\other;%DS_CLI%\vendor\wp-cli\wp-cli\bin;%DS_CLI%\platform\win32\nodejs\;%PATH%
 %*
