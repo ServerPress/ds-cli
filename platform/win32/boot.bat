7be48589b876b77a4d928b2b7b4e2b5c134b5812
Stephen J. Carnam <steve@steveorevo.com>
2016-08-03 10:43:13 -0700

    Fix composer and git clone with SSL

--- platform/win32/boot.bat
+++ platform/win32/boot.bat
@@ -9,5 +9,7 @@ SET npm_config_cache=%DS_CLI%\platform\win32\npm-cache
 SET npm_config_prefix=%DS_CLI%\platform\win32\npm
 SET COMPOSER_HOME=%DS_CLI%\platform\win\composer
 SET CYGWIN_HOME=%DS_CLI%\platform\win32\cygwin
+SET GIT_SSL_CAPATH=%DS_CLI%\platform\win32\cygwin\usr\ssl\certs
+SET GIT_SSL_CAINFO=%DS_CLI%\platform\win32\cygwin\etc\pki\ca-trust\extracted\pem\tls-ca-bundle.pem
 PATH=%DS_CLI%\platform\all\pre;%DS_CLI%\platform\win32\pre;%DS_CLI%\platform\win32\cygwin\bin;%DS_CLI%\platform\win32\nodejs;%DS_CLI%\platform\win32\npm;c:\xampplite\php;c:\xampplite\mysql\bin;c:\xampplite\perl\bin;c:\xampplite\apache\bin;c:\xampplite\other;%DS_CLI%\vendor\wp-cli\wp-cli\bin;%DS_CLI%\platform\win32\nodejs\;%PATH%
 %*
