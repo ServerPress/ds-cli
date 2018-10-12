1400e7a79001dc8cf89b6749100077f2c6d63a20
Stephen J. Carnam <steve@steveorevo.com>
2018-10-12 10:01:50 -0700

    npm path correction for mac

--- src/platform/mac/boot.sh
+++ src/platform/mac/boot.sh
@@ -13,6 +13,6 @@ export npm_config_cache="$DS_CLI/platform/mac/npm-cache"
 export npm_config_prefix="$DS_CLI/platform/mac/npm"
 export COMPOSER_HOME="$DS_CLI/platform/mac/composer"
 export DYLD_FALLBACK_LIBRARY_PATH="$DS_CLI/platform/mac/homebrew/lib:/Applications/XAMPP/xamppfiles/lib:/usr/lib"
-export PATH="$DS_CLI/platform/all/pre:$DS_CLI/platform/mac/pre:$DS_CLI/vendor/bin:$DS_CLI/platform/mac/homebrew/bin:$DS_CLI/platform/mac/nodejs/bin:$DS_CLI/platform/mac/npm:/Applications/XAMPP/xamppfiles/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:$PATH"
+export PATH="$DS_CLI/platform/all/pre:$DS_CLI/platform/mac/pre:$DS_CLI/vendor/bin:$DS_CLI/platform/mac/homebrew/bin:$DS_CLI/platform/mac/nodejs/bin:$DS_CLI/platform/mac/npm/bin:/Applications/XAMPP/xamppfiles/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:$PATH"
 unset DYLD_LIBRARY_PATH
 "$@"
