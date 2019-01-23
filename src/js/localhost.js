19368425b45b8403b793cef9a1343dad7f6f0f3a
Dave Jesch <davejesch@gmail.com>
2019-01-22 17:23:51 -0800

    Update localhost.js
    update class references for click event handler #27

--- src/js/localhost.js
+++ src/js/localhost.js
@@ -1,11 +1,11 @@
 (function($){
-	$('.dev-sites tr').hover(function() {
-		$(this).find('.ds-site-actions-container').show();
-	}, function() {
-		$(this).find('.ds-site-actions-container').hide();
-	});
+//	$('.dev-sites tr').hover(function() {
+//		$(this).find('.ds-site-actions-container').show();
+//	}, function() {
+//		$(this).find('.ds-site-actions-container').hide();
+//	});
 
-	$('.dev-sites .ds-site-actions-container .ds-action.ds-cli').on('click', function(e) {
+	$('.dev-sites .btn-group .ds-cli').on('click', function(e) {
 		e.preventDefault();
 		$.post($(this).attr('href'), {
 			domain: $(this).data('domain')
