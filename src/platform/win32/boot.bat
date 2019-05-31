1e2ca8bc78f21b62859ad6e0480e004c3881bda4
Stephen J. Carnam <steve@steveorevo.com>
2019-05-30 19:51:21 -0700

    Fix cd command to home folder

--- src/platform/win32/boot.bat
+++ src/platform/win32/boot.bat
@@ -9,5 +9,6 @@ SET npm_config_cache=%DS_CLI%\platform\win32\npm-cache
 SET npm_config_prefix=%DS_CLI%\platform\win32\npm
 SET COMPOSER_HOME=%DS_CLI%\platform\win32\composer
 SET CYGWIN_HOME=%DS_CLI%\platform\win32\cygwin
+SET HOME=%USERPROFILE%
 PATH=%DS_CLI%\platform\all\pre;%DS_CLI%\platform\win32\pre;%DS_CLI%\platform\win32\cygwin\bin;%DS_CLI%\platform\win32\nodejs;%DS_CLI%\platform\win32\npm;c:\xampplite\php;c:\xampplite\mysql\bin;c:\xampplite\perl\bin;c:\xampplite\apache\bin;c:\xampplite\other;%DS_CLI%\platform\all\wp-cli\bin;%PATH%
 %*
